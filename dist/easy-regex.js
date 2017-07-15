342eef9482ae4a2579774ff5e0170710a3405b62
George Valtas <grvaltas@gmail.com>
2017-07-16 00:53:54 +0300

    Update easy-regex.js

--- dist/easy-regex.js
+++ dist/easy-regex.js
@@ -1,81 +1,107 @@
-var ignoredSymbols = "[\\*\\^\\$]";
+var ignoredSymbols = "[\\*\\^\\$]"
 
-function has(type, input) {
+function has(string, input) {
 	if (!input) {
-		if (type == "numbers" || type == "digits" || type == "num") return '(?=.*[0-9])';
-		if (type == "letters" || type == "char") return '(?=.*[a-zA-Z])';
+		if (string == "numbers" || string == "digits" || string == "num") return '(?=.*[0-9])'
+		if (string == "letters" || string == "char") return '(?=.*[a-zA-Z])'
 		
-		if (type == "$numbers") return '(?=.*\\b' + type.substring(1) + '\\b.*)';
-		if (type == "$letters") return '(?=.*\\b' + type.substring(1) + '\\b.*)';
+		if (string == "$numbers" || string == "$digits" || string == "$num" || string == "$letters" || string == "$char") {
+			return '(?=.*\\b' + string.substring(1) + '\\b.*)'
+		}
 
-		if (type.match(ignoredSymbols)) {
-			for (i = 0; i < type.match(new RegExp('[\\*\\^\\$]', 'g')).length; i++) {
-				var metaChar = type.match(new RegExp('[\\*\\^\\$]', 'g'))[i];
-				var type = type.replace(metaChar, "\\" + metaChar);
+		if (string.match(ignoredSymbols)) {
+			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+				    string = string.replace(metaChar, "\\" + metaChar)
 			}
-			return new RegExp(type, 'g')
+			return new RegExp(string)
 		} else {
-			return '(?=.*\\b' + type + '\\b.*)'
+			return '(?=.*\\b' + string + '\\b.*)'
 		}
 	} else {
-		return input.match(new RegExp(type, 'gi')).length
+		if (string.match(ignoredSymbols)) {
+			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+				    string = string.replace(metaChar, "\\" + metaChar)
+			}
+		}
+		return input.match(new RegExp(string, 'gi')).length			
 	}
 }
-function notString(string) {
+function not(string) {
+	if (string == "numbers" || string == "digits" || string == "num") return '(?=.*^([^0-9]*)$)'
+	if (string == "letters" || string == "char") return '(?=.*^([^a-zA-Z]*)$)'
+
+	if (string == "$numbers" || string == "$digits" || string == "$num" || string == "$letters" || string == "$char") {
+		return '(?=.*^(?!.*' + string.substring(1) + ').*$)'
+	}
+
 	if (string.match(ignoredSymbols)) {
-		for (i = 0; i < string.match(new RegExp('[\\*\\^\\$]', 'g')).length; i++) {
-			var metaChar = string.match(new RegExp('[\\*\\^\\$]', 'g'))[i]
-			var string = string.replace(metaChar, "\\" + metaChar)
+		for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+			var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+				string = string.replace(metaChar, "\\" + metaChar)
 		}
-		return new RegExp('.*^(?!.*' + string + ').*$', 'g')
-	} 
+	}
 	return '(?=.*^(?!.*' + string + ').*$)'
 }
-function not(type) {
-	if (type == "numbers" || type == "digits" || type == "num") return '(?=.*^([^0-9]*)$)';
-	if (type == "letters" || type == "char") return '(?=.*^([^a-zA-Z]*)$)';
-	if (type.match(ignoredSymbols)) {
-		for (i = 0; i < type.match(new RegExp('[\\*\\^\\$]', 'g')).length; i++) {
-			var metaChar = type.match(new RegExp('[\\*\\^\\$]', 'g'))[i];
-			var type = type.replace(metaChar, "\\" + metaChar);
+function beginsWith(string, input) {
+	if (!input) {
+		if (string.match(ignoredSymbols)) {
+			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+					string = string.replace(metaChar, "\\" + metaChar)
+			}
 		}
-		return new RegExp('.*^(?!.*' + type + ').*$', 'g')
+		return '(?=\\b' + string + ')'
 	} else {
-		return '(?=.*^(?!.*' + type + ').*$)'
+		if (string.match(ignoredSymbols)) {
+			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+					string = string.replace(metaChar, "\\" + metaChar)
+			}
+		}
+		return input.match(new RegExp('\\b' + string, 'g')).length
 	}
 }
-function beginsWith(type, input) {
+function endsWith(string, input) {
 	if (!input) {
-		return '(?=\\b' + type + ')'
+		if (string.match(ignoredSymbols)) {
+			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+					string = string.replace(metaChar, "\\" + metaChar)
+			}
+		}
+		return '(?=\\B' + string + ')'
 	} else {
-		return input.match(new RegExp('\\b' + type, 'g')).length
-	}
-}
-function endsWith(type, input) {
-	if (!input) {
-		return '(?=\\B' + type + ')'
-	} else {
-		return input.match(new RegExp('\\B' + type, 'g')).length
+		if (string.match(ignoredSymbols)) {
+			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+					string = string.replace(metaChar, "\\" + metaChar)
+			}
+		}
+		return input.match(new RegExp('\\B' + string, 'g')).length
 	}
 }
 function wordSize(size, input) {
 	if (!input) {
 		return '(?=[a-zA-Z0-9]{' + size + '})'
 	} else {
-		return input.match(new RegExp('[a-z0-9]{' + size + '}', 'gi')).length
+		return input.match(new RegExp('[a-z0-9]{' + size + '}', 'gi')).length	
 	}
 }
 function or() {
 	return '|'
 }
 // does not work with or() and + 
-function atLeastOne(type, input) {
-	if (!input) {
-		return new RegExp('(?=' + type + '+)', 'g')
-	} else {
-		return input.match(new RegExp('(?=' + type + '+)', 'g')).length
+function atLeastOne(string) {
+	if (string.match(ignoredSymbols)) {
+		for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+			var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
+				string = string.replace(metaChar, "\\" + metaChar)
+		}
 	}
+	return new RegExp('(?=' + string + '+)', 'g')
 }
-function showRegex(type) { 
-	return eval(type)
+function showRegex(string) { 
+	return eval(string)
 }
