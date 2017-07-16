6d8f7553b05a78152f6db60d1de09ecf89d2d654
George Valtas <grvaltas@gmail.com>
2017-07-16 14:59:23 +0300

    Update easy-regex.js

--- dist/easy-regex.js
+++ dist/easy-regex.js
@@ -1,28 +1,26 @@
+//meta-characters that bring errors when used in a function's parameter
 var ignoredSymbols = "[\\*\\^\\$]"
 
 function has(string, input) {
 	if (!input) {
 		if (string == "numbers" || string == "digits" || string == "num") return '(?=.*[0-9])'
 		if (string == "letters" || string == "char") return '(?=.*[a-zA-Z])'
-		
+
 		if (string == "$numbers" || string == "$digits" || string == "$num" || string == "$letters" || string == "$char") {
 			return '(?=.*\\b' + string.substring(1) + '\\b.*)'
 		}
-
+		//loop that checks for meta chars and adds a backlash for everyone that is found
 		if (string.match(ignoredSymbols)) {
 			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-				    string = string.replace(metaChar, "\\" + metaChar)
+				//replaces each meta-char with '\\' to prevent errors
+				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 			}
-			return new RegExp(string)
-		} else {
-			return '(?=.*\\b' + string + '\\b.*)'
 		}
+		return '(?=.*\\b' + string + '\\b.*)'
 	} else {
 		if (string.match(ignoredSymbols)) {
 			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-				    string = string.replace(metaChar, "\\" + metaChar)
+				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 			}
 		}
 		return input.match(new RegExp(string, 'gi')).length			
@@ -38,8 +36,7 @@ function not(string) {
 
 	if (string.match(ignoredSymbols)) {
 		for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-			var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-				string = string.replace(metaChar, "\\" + metaChar)
+			string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 		}
 	}
 	return '(?=.*^(?!.*' + string + ').*$)'
@@ -48,16 +45,14 @@ function beginsWith(string, input) {
 	if (!input) {
 		if (string.match(ignoredSymbols)) {
 			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-					string = string.replace(metaChar, "\\" + metaChar)
+				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 			}
 		}
 		return '(?=\\b' + string + ')'
 	} else {
 		if (string.match(ignoredSymbols)) {
 			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-					string = string.replace(metaChar, "\\" + metaChar)
+				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 			}
 		}
 		return input.match(new RegExp('\\b' + string, 'g')).length
@@ -67,16 +62,14 @@ function endsWith(string, input) {
 	if (!input) {
 		if (string.match(ignoredSymbols)) {
 			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-					string = string.replace(metaChar, "\\" + metaChar)
+				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 			}
 		}
 		return '(?=\\B' + string + ')'
 	} else {
 		if (string.match(ignoredSymbols)) {
 			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-					string = string.replace(metaChar, "\\" + metaChar)
+				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 			}
 		}
 		return input.match(new RegExp('\\B' + string, 'g')).length
@@ -96,8 +89,7 @@ function or() {
 function atLeastOne(string) {
 	if (string.match(ignoredSymbols)) {
 		for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-			var metaChar = string.match(new RegExp(ignoredSymbols, 'g'))[i]
-				string = string.replace(metaChar, "\\" + metaChar)
+			string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
 		}
 	}
 	return new RegExp('(?=' + string + '+)', 'g')
