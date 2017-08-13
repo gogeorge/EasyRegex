bbcfe669eb321c39f3ba40abf7f3085857e91385
George Valtas <grvaltas@gmail.com>
2017-08-13 20:42:26 +0300

    Add files via upload

--- distributable/easy-regex.js
+++ distributable/easy-regex.js
@@ -1,96 +1,362 @@
-//meta-characters that bring errors when used in a function's parameter
-var ignoredSymbols = "[\\*\\^\\$]"
+/*
+	* Author: George Valtas
+	* released under the MIT License
+	* v2.0
+	* 2017
+*/
 
-function has(string, input) {
-	if (!input) {
-		if (string == "numbers" || string == "digits" || string == "num") return '(?=.*[0-9])'
-		if (string == "letters" || string == "char") return '(?=.*[a-zA-Z])'
+var regexSymbols = '[\\*\\^\\$]'
 
-		if (string == "$numbers" || string == "$digits" || string == "$num" || string == "$letters" || string == "$char") {
-			return '(?=.*\\b' + string.substring(1) + '\\b.*)'
-		}
-		//loop that checks for meta chars and adds a backlash for everyone that is found
-		if (string.match(ignoredSymbols)) {
-			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
+String.prototype.has = function( string, length ) {
+	var variable = ''
+	/*
+		the loop gets all the characters of the variable which the .has() method 
+		is assigned to and forms a string which is stored in 'variable'
+	*/
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+	if ( !length ) {
+		var or = string.match(/\sor\s/g)
+		var and = string.match(/\sand\s/g)
+
+		if ( !or ) or = ''
+		if ( !and ) and = ''
+
+		var _and = string.match(/\s_and\s/g)
+		var _or = string.match(/\s_or\s/g)
+
+		if ( !_and ) _and = ''
+		if ( !_or ) _or = ''
+
+		if ( string.match(regexSymbols) ) {
+			for ( i = 0; i < string.match(new RegExp(regexSymbols, 'g')).length; i++ ) {
 				//replaces each meta-char with '\\' to prevent errors
-				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
-			}
-		}
-		return '(?=.*\\b' + string + '\\b.*)'
-	} else {
-		if (string.match(ignoredSymbols)) {
-			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
-			}
-		}
-		return input.match(new RegExp(string, 'gi')).length			
-	}
-}
-function not(string) {
-	if (string == "numbers" || string == "digits" || string == "num") return '(?=.*^([^0-9]*)$)'
-	if (string == "letters" || string == "char") return '(?=.*^([^a-zA-Z]*)$)'
+				var currentSymbols = string.match(new RegExp(regexSymbols, 'g'))[i]
+				var backlashedSymbols = "\\" + string.match(new RegExp(regexSymbols, 'g'))[i]
 
-	if (string == "$numbers" || string == "$digits" || string == "$num" || string == "$letters" || string == "$char") {
-		return '(?=.*^(?!.*' + string.substring(1) + ').*$)'
+				string = string.replace(currentSymbols, backlashedSymbols)
+			}
+		}
+		// if user has used the word 'or', replace it with with a pipe
+		if ( or ) {
+			for ( i = 0; i < or.length; i++ ) {
+				string = string.replace(' or ', '|')
+			}
+		}
+
+		if ( and ) {
+			for ( i = 0; i < and.length; i++ ) {
+				string = string.replace(' and ', '\\b.*)(?=.*\\b')
+			}
+		}
+
+		if ( string.match(/_and|_or/) ) {
+			for ( i = 0; i < _or.length + _and.length; i++ ) {
+				string = string.replace(/_and/, 'and')
+				string = string.replace(/_or/, 'or')
+			}
+		}
+		/*
+			if the string contains '?=' because 'var and' is true 
+			then add the following regex together with the string 
+		*/
+		if ( string.includes('?=') ) {
+			return variable.match('(?=.*\\b' + string + '\\b.*)')
+		}
+	}
+	// this is needed when the occurrences() function is used
+	else if ( length == 'length') {
+		return variable.match(new RegExp('\\b' + string + '\\b', 'g')).length
 	}
 
-	if (string.match(ignoredSymbols)) {
-		for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-			string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
+	return variable.match('.*\\b' + string + '\\b.*')
+}
+
+String.prototype.not = function( string ) {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	var or = string.match(/\sor\s/g)
+	var and = string.match(/\sand\s/g)
+	var nor = string.match(/\snor\s/g)
+
+	if ( !or ) or = ''
+	if ( !and ) and = ''
+	if ( !nor ) nor = ''
+	
+	var _or = string.match(/\s_or\s/g)
+	var _and = string.match(/\s_and\s/g)
+	var _nor = string.match(/\s_nor\s/g)
+	
+	if ( !_or ) _or = ''
+	if ( !_and ) _and = ''
+	if ( !_nor ) _nor = ''
+
+	if ( string.match(regexSymbols) ) {
+		for ( i = 0; i < string.match(new RegExp(regexSymbols, 'g')).length; i++ ) {
+			var currentSymbols = string.match(new RegExp(regexSymbols, 'g'))[i]
+			var backlashedSymbols = "\\" + string.match(new RegExp(regexSymbols, 'g'))[i]
+
+			string = string.replace(currentSymbols, backlashedSymbols)
 		}
 	}
-	return '(?=.*^(?!.*' + string + ').*$)'
+
+	if ( or ) {
+		for ( i = 0; i < or.length; i++ ) {
+			string = string.replace(' or ', ').*$)|(.*^(?!.*')
+		}
+	}
+
+	if ( and || nor ) {
+		for ( i = 0; i < and.length + nor.length; i++ ) {
+			string = string.replace(' and ', ').*$)(?=.*^(?!.*')
+			string = string.replace(' nor ', ').*$)(?=.*^(?!.*')
+		}
+	}
+
+	if ( string.match(/_and|_or|_nor/) ) {
+		for ( i = 0; i < _or.length + _and.length; i++ ) {
+			string = string.replace(/_and/, 'and')
+			string = string.replace(/_or/, 'or')
+			string = string.replace(/_nor/, 'nor')
+		}
+	}
+
+	if ( string.includes('?=') ) {
+		return variable.match('(?=.*^(?!.*' + string + ').*$)')
+	}
+
+	return variable.match(new RegExp('(.*^(?!.*' + string + ').*$)', 'gi'))
 }
-function beginsWith(string, input) {
-	if (!input) {
-		if (string.match(ignoredSymbols)) {
-			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
+
+String.prototype.beginsWith = function( string, length, not ) {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	if ( !length && !not ) {
+		var or = string.match(/\sor\s/g)
+		var and = string.match(/\sand\s/g)
+
+		if ( !or ) or = ''
+		if ( !and ) and = ''
+
+		var _and = string.match(/\s_and\s/g)
+		var _or = string.match(/\s_or\s/g)
+
+		if ( !_and ) _and = ''
+		if ( !_or ) _or = ''
+
+		if ( string.match(regexSymbols) ) {
+			for ( i = 0; i < string.match(new RegExp(regexSymbols, 'g')).length; i++ ) {
+				var currentSymbols = string.match(new RegExp(regexSymbols, 'g'))[i]
+				var backlashedSymbols = "\\" + string.match(new RegExp(regexSymbols, 'g'))[i]
+
+				string = string.replace(currentSymbols, backlashedSymbols)
 			}
 		}
-		return '(?=\\b' + string + ')'
-	} else {
-		if (string.match(ignoredSymbols)) {
-			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
+
+		if ( or ) {
+			for ( i = 0; i < or.length; i++ ) {
+				string = string.replace(' or ', ')|(\\b')
 			}
 		}
-		return input.match(new RegExp('\\b' + string, 'g')).length
-	}
-}
-function endsWith(string, input) {
-	if (!input) {
-		if (string.match(ignoredSymbols)) {
-			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
+
+		if ( and ) {
+			for ( i = 0; i < and.length; i++ ) {
+				string = string.replace(' and ', ')(?=.*\\b')
 			}
 		}
-		return '(?=\\B' + string + ')'
-	} else {
-		if (string.match(ignoredSymbols)) {
-			for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-				string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
+
+		if ( string.match(/_and|_or/) ) {
+			for ( i = 0; i < _or.length + _and.length; i++ ) {
+				string = string.replace(/_and/, 'and')
+				string = string.replace(/_or/, 'or')
 			}
 		}
-		return input.match(new RegExp('\\B' + string, 'g')).length
-	}
-}
-function wordSize(size, input) {
-	if (!input) {
-		return '(?=[a-zA-Z0-9]{' + size + '})'
-	} else {
-		return input.match(new RegExp('[a-z0-9]{' + size + '}', 'gi')).length	
-	}
-}
-function or() {
-	return '|'
-}
-// does not work with or() and + 
-function atLeastOne(string) {
-	if (string.match(ignoredSymbols)) {
-		for (i = 0; i < string.match(new RegExp(ignoredSymbols, 'g')).length; i++) {
-			string = string.replace(string.match(new RegExp(ignoredSymbols, 'g'))[i], "\\" + string.match(new RegExp(ignoredSymbols, 'g'))[i])
+
+		if ( string.includes('?=') ) {
+			return variable.match(new RegExp('(?=.*\\b' + string + ')', 'g'))
 		}
+
+		return variable.match('(\\b' + string + ')')
 	}
-	return new RegExp('(?=' + string + '+)', 'g')
+
+	else if ( length == 'length' ) {
+		return variable.match(new RegExp('\\b' + string, 'g')).length
+	}
+
+	else if ( not == 'not' ) {
+		return variable.match('.*^(?!.*\\b' + string + ').*$')
+	}
+}
+
+String.prototype.endsWith = function( string, length, not ) {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	if ( !length && !not ) {
+		var or = string.match(/\sor\s/g)
+		var and = string.match(/\sand\s/g)
+
+		if ( !or ) or = ''
+		if ( !and ) and = ''
+
+		var _and = string.match(/\s_and\s/g)
+		var _or = string.match(/\s_or\s/g)
+
+		if ( !_and ) _and = ''
+		if ( !_or ) _or = ''
+
+		if ( string.match(regexSymbols) ) {
+			for ( i = 0; i < string.match(new RegExp(regexSymbols, 'g')).length; i++ ) {
+				var currentSymbols = string.match(new RegExp(regexSymbols, 'g'))[i]
+				var backlashedSymbols = "\\" + string.match(new RegExp(regexSymbols, 'g'))[i]
+
+				string = string.replace(currentSymbols, backlashedSymbols)
+			}
+		}
+
+		if ( or ) {
+			for ( i = 0; i < or.length; i++ ) {
+				string = string.replace(' or ', ')|(\\B')
+			}
+		}
+
+		if ( and ) {
+			for ( i = 0; i < and.length; i++ ) {
+				string = string.replace(' and ', ')(?=.*\\B')
+			}
+		}
+
+		if ( string.match(/_and|_or/) ) {
+			for ( i = 0; i < _or.length + _and.length; i++ ) {
+				string = string.replace(/_and/, 'and')
+				string = string.replace(/_or/, 'or')
+			}
+		}
+
+		if ( string.includes('?=') ) {
+			return variable.match('(?=.*\\B' + string + ')')
+		}
+
+		return variable.match('(\\B' + string + ')')
+	}
+
+	else if ( length == 'length' ) {
+		return variable.match(new RegExp('\\B' + string, 'g')).length
+	}
+
+	else if ( not == 'not' ) {
+		return variable.match('.*^(?!.*\\B' + string + ').*$')
+	}
+}
+
+String.prototype.wordSize = function( string, length, not ) {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	if ( !length && !not && string.not('from nor to') ) {
+		var or = string.match(/\sor\s/g)
+
+		if ( !or ) or = ''
+
+		if ( or ) {
+			for ( i = 0; i < or.length; i++ ) {
+				string = string.replace(' or ', '})(\\b)|(\\b)(\\w{')
+			}
+
+			return variable.match('(\\b)(\\w{' + string + '})(\\b)')
+		}
+
+		return variable.match('\\w{' + string + '}')
+	}
+	else if ( !length && !not && string.has('from and to') ) {
+		var formerDigit = string.substring(string.indexOf('from') + 5, string.indexOf('to') - 1)
+		var latterDigit = string.substring(string.indexOf('to') + 3)
+
+		return variable.match('\\w{' + formerDigit + ',' + latterDigit + '}')
+	}
+
+	else if ( length == 'length' ) {
+		return variable.match(new RegExp('(\\b)(\\w{' + string + '})(\\b)', 'g')).length
+	} 
+
+	else if ( not == 'not' ) {
+		return variable.match(new RegExp('(.*^(?!.*\\w{' + string + '}).*$)', 'g'))
+	} 
+}
+
+String.prototype.hasLetters = function() {  
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	return variable.match('.*[a-zA-Z]')
+}
+
+String.prototype.hasNumbers = function() {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	return variable.match('.*[0-9]')
+}
+
+String.prototype.hasCaps = function() {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	return variable.match('.*[A-Z]')
+}
+
+String.prototype.hasNoCaps = function() {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+
+	return variable.match('.*^([^A-Z]*)$') 
+}
+
+String.prototype.hasOnlyCaps = function() {
+	var variable = ''
+
+	for ( i = 0; i < this.length; i++ ) {
+		variable += this[ i ]
+	}
+	
+	return variable.match('.*^([^a-z]*)$')
+}
+
+function occurrences( fn ) {
+	fn = fn.substring(0, fn.lastIndexOf(')'))
+	fn = fn + ", 'length')"
+	return eval( fn )
+}
+
+function not( fn ) {
+	fn = fn.substring(0, fn.lastIndexOf(')'))
+	fn = fn + ", 'null', 'not')"
+	return eval( fn )
 }
